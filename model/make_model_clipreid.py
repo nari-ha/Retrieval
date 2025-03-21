00fd4c8641092fe180bb230f5b813e58262a1c87
nari-ha <narihanar@gmail.com>
2025-03-21 11:31:18 +0000

    .

--- model/make_model_clipreid.py
+++ model/make_model_clipreid.py
@@ -225,8 +225,9 @@ class PromptLearner(nn.Module):
         print("라벨: ", label)
         label = torch.tensor(label)
         print("라벨 사이즈: ", label.size())
-        print("cls_ctx: ", cls_ctx.size())
+
         cls_ctx = self.cls_ctx[label] 
+        print("cls_ctx: ", cls_ctx.size())
         b = label.shape[0]
         prefix = self.token_prefix.expand(b, -1, -1) 
         suffix = self.token_suffix.expand(b, -1, -1) 
